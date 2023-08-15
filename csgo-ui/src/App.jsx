93d58cf5230b5044e3328d215020686f6923d623
FatihAksoy530 <fatihaksoy1999@hotmail.com>
2023-08-15 03:43:31 +0300

    rarity scaler added

--- csgo-ui/src/App.jsx
+++ csgo-ui/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Spinner from './components/spinner/Spinner';
+import RarityScaler from './components/rarity_scaler/RarityScaler';
 
 import './assets/css/global.css'
 import './assets/css/css_reset.css'
@@ -10,7 +11,8 @@ function App() {
   return (
     <>
       <div id='main-layout-container'>
-        <Spinner className={ "spinner" } />
+        <Spinner/>
+        <RarityScaler />
       </div>
     </>
   )
---
+++ csgo-ui/src/components/rarity_scaler/RarityScaler.jsx
@@ -0,0 +1,14 @@
+import "./RarityScaler.css";
+
+export default function RarityScaler() {
+    return (
+        <>
+            <div className="rarity-scaler">
+                <div className="unusual"></div>
+                <div className="rare"></div>
+                <div className="common"></div>
+                <div className="uncommon"></div>
+            </div>
+        </>
+    )
+}
